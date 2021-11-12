6645a6825093c14b74cada82a1d5d49f01c5769e
Jazim Latif <jazimlatif@Jazims-Air.broadband>
2021-11-12 09:49:41 +0000

    editing sql table slightly to fix spelling errors

--- src/main/resources/db/migration/V1__Initial_Setup.sql
+++ src/main/resources/db/migration/V1__Initial_Setup.sql
@@ -1,11 +1,11 @@
 CREATE TABLE songs (id SERIAL PRIMARY KEY, mood VARCHAR(255), artist VARCHAR(255), song_name VARCHAR(255));
 
-CREATE TABLE playlists (id SERIAL PRIMARY KEY, playlist_user INT REFERENCES users(id));
+CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR(255), email VARCHAR(255) );
 
-CREATE TABLE playlist_songs (id SERIAL PRIMARY KEY, playlist_id INT REFERENCES playlist(id),
+CREATE TABLE playlist (id SERIAL PRIMARY KEY, playlist_user INT REFERENCES users(id));
+
+CREATE TABLE playlist_maker (id SERIAL PRIMARY KEY, playlist_id INT REFERENCES playlist(id),
  song_id INT REFERENCES song(id));
-
-CREATE TABLE users ( id SERIAL PRIMARY KEY, username VARCHAR(255), email VARCHAR(255) );
 
 CREATE TABLE quizzes (id SERIAL PRIMARY KEY, title VARCHAR(255));
 
@@ -16,4 +16,4 @@ CREATE TABLE options (id SERIAL PRIMARY KEY, question_id INT REFERENCES question
  option_text VARCHAR(255), option_mood VARCHAR(255));
 
 CREATE TABLE user_answers (id SERIAL PRIMARY KEY, user_id INT REFERENCES users(id),
- question_id INT REFERENCES question(id), choice_id INT REFERENCES options(id));
+ question_id INT REFERENCES questions(id), option_id INT REFERENCES options(id));
