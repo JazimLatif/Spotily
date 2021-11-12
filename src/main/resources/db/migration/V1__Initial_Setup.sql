817840e694dbb1a142e4adf3055905203706d3e8
Jazim Latif <jazimlatif@Jazims-Air.broadband>
2021-11-12 10:54:09 +0000

    Updating SQL table to remove tables we didnt need

--- src/main/resources/db/migration/V1__Initial_Setup.sql
+++ src/main/resources/db/migration/V1__Initial_Setup.sql
@@ -5,15 +5,22 @@ CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR(255), email VARCHAR(
 CREATE TABLE playlist (id SERIAL PRIMARY KEY, playlist_user INT REFERENCES users(id));
 
 CREATE TABLE playlist_maker (id SERIAL PRIMARY KEY, playlist_id INT REFERENCES playlist(id),
- song_id INT REFERENCES song(id));
+ song_id INT REFERENCES songs(id));
 
-CREATE TABLE quizzes (id SERIAL PRIMARY KEY, title VARCHAR(255));
-
-CREATE TABLE questions (id SERIAL PRIMARY KEY, question_text VARCHAR(255),
- quiz_id INT REFERENCES quizzes(id));
+CREATE TABLE questions (id SERIAL PRIMARY KEY, question_text VARCHAR(255));
 
 CREATE TABLE options (id SERIAL PRIMARY KEY, question_id INT REFERENCES questions(id),
  option_text VARCHAR(255), option_mood VARCHAR(255));
 
+/*
 CREATE TABLE user_answers (id SERIAL PRIMARY KEY, user_id INT REFERENCES users(id),
  question_id INT REFERENCES questions(id), option_id INT REFERENCES options(id));
+
+ CREATE TABLE quizzes (id SERIAL PRIMARY KEY, title VARCHAR(255));
+
+*/
+
+/*
+Maybe need an inner join between song and options linked via mood
+Might not need quiz and user_answers because can just query between other tables
+*/
